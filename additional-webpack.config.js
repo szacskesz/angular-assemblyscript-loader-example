5b3db190425b0c83fd22c17266ca59dfd6306271
Szacsuri Norbert <szacsuri.norbert@virgo.hu>
2022-10-30 09:01:39 +0100

    NG additional webpack config

---
+++ additional-webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+    module: {
+        rules: [
+            // {
+            //   test: /\.ts$/,
+            //   include: [path.resolve(__dirname, "src/assembly")],
+            //   loader: "as-loader",
+            //   options: {
+            //     bind: true,
+            //   },
+            // },
+            // {
+            //   test: /\.ts$/,
+            //   exclude: [path.resolve(__dirname, "src/assembly")],
+            //   loader: "ts-loader",
+            // },
+        ]
+    }
+};
